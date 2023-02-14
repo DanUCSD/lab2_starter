7dc02f8fc7ed39b24adf9bebd384203918a8e758
DanUCSD <103155873+DanUCSD@users.noreply.github.com>
2023-02-13 16:00:17 -0800

    starter part 3

--- Part3/top_level_lab2_part3.sv
+++ Part3/top_level_lab2_part3.sv
@@ -1,27 +1,121 @@
 // CSE140L  
 // see Structural Diagram in Lab2 Part 3assignment writeup
 // fill in missing connections and parameters
-module top_level_lab2_part3(
+module top_level_lab2_part2(
   input Reset,
-        Timeset,    // manual buttons
-        Alarmset,   //  (five total)
-		  Minadv,
-		  Hrsadv,
+        Timeset, 	  // manual buttons
+        Alarmset,	  //	(five total)
+        Minadv,
+        Hrsadv,
         Dayadv,
         Monthadv,
         Dateadv,
-    Alarmon,
-    Pulse,      // assume 1/sec.
-    DorT,
+        Alarmon,
+        Pulse,		  // assume 1/sec.
+        DorT			   
 // 6 decimal digit display (7 segment)
-  output[6:0] S1disp, S0disp,      // 2-digit  display
-              MD1disp, MD0disp,    // 2 digit display  minutes/date
-              HM1disp, HM0disp,     // 2-digit display hours/month
-               DayLED,             // day of week LED
-   // date display
-  output logic AMorPM,              
-  output logic Buzz);            // alarm sounds
+  output[6:0] S1disp, S0disp, 	   // 2-digit seconds display
+              M1disp, M0disp, 
+              H1disp, H0disp,
+              DayLED,
+  output logic AMorPM,            // Added by Arpita 
+  output logic Buzz);	           // alarm sounds
+  
+   logic [6:0] TSec, TMin, THrs;   // time 
+   logic       TPm;                // time PM
+   logic [6:0] AMin, AHrs;         // alarm setting
+   logic       APm;                // alarm PM
+   logic [2:0] TDay;
+   
+     
+  logic[6:0] Min, Hrs;                     // drive Min and Hr displays
+  logic Smax, Mmax, Hmax, Dmax,         // "carry out" from sec -> min, min -> hrs, hrs -> days
+        TMen, THen, TPmen, AMen, AHen, AHmax, AMmax, APmen,    // respective counter enables
+        Dayen;
+        logic         Buzz1;             // intermediate Buzz signal
+
+   ct_mod_N #(.N()) Sct(
+         .clk(Pulse), .rst(Reset), .en(!Timeset), .ct_out(TSec), .z(Smax)    
+   );
+
+        // if Smax is true (if we need to increment minutes), then run this
+   assign TMen = Smax || (Timeset && Minadv);
+   ct_mod_N #(.N()) Mct(
+         .clk(Pulse), .rst(Reset), .en(TMen), .ct_out(TMin), .z(Mmax)
+   );
 
 
-//... Fill in with part3 implementation
+   assign THen = (Mmax && Smax) || (Timeset && Hrsadv) || (Timeset && Mmax && Minadv);  // It resets to 00 instead of staying at 12.
+   ct_mod_N #(.N(12)) Hct(                          
+         .clk(Pulse), .rst(Reset), .en(THen), .ct_out(THrs), .z(Hmax)
+   );
+
+
+   assign TPmen = (Smax && Mmax && Hmax) || (Timeset && Hmax && Hrsadv) || (Timeset && Hmax && Mmax && Minadv);
+   regce TPMct(.out(TPm), .inp(!TPm), .en(TPmen),
+               .clk(Pulse), .rst(Reset));
+
+
+  assign AMen = Alarmset && Minadv;
+  ct_mod_N #(.N()) Mreg(
+    .clk(Pulse), .rst(Reset), .en(AMen), .ct_out(AMin), .z(AMmax)
+   ); 
+
+  assign AHen = Alarmset && ((Minadv && AMmax) || Hrsadv);
+  ct_mod_N #(.N(12)) Hreg(          
+    .clk(Pulse), .rst(Reset), .en(AHen), .ct_out(AHrs), .z(AHmax)
+  ); 
+
+   // alarm AM/PM state 
+   assign APmen = Alarmset && ((AHmax && Minadv && AMmax) || (AHmax && Hrsadv));
+   regce APMReg(.out(APm), .inp(!APm), .en(APmen),
+               .clk(Pulse), .rst(Reset));
+
+   // display drivers (2 digits each, 6 digits total)
+   lcd_int Sdisp(
+    .bin_in    (TSec)  ,
+        .Segment1  (S1disp),
+        .Segment0  (S0disp)
+   );
+
+   lcd_int Mdisp(
+    .bin_in    (Alarmset ? AMin: TMin) ,
+        .Segment1  (M1disp),
+        .Segment0  (M0disp)
+        );
+
+  lcd_int Hdisp(
+    .bin_in    (Alarmset ? AHrs == 0 ? 12 : AHrs: THrs == 0 ? 12: THrs),
+        .Segment1  (H1disp),
+        .Segment0  (H0disp)
+        );
+        
+   alarm a1(
+           .tmin(TMin), .amin(AMin), .thrs(THrs), .ahrs(AHrs), .tpm(TPm), .apm(APm), .buzz(Buzz1)
+           );
+
+	assign Buzz = Alarmon ? Buzz1 : 0;
+ 
+	assign AMorPM = Alarmset ? APm : TPm;
+
+  // Day LED, Dayen'ed when dayadv or (pm and (natural roll over or hrsadv roll over or minadv roll over))
+
+  assign Dayen = Dayadv || (TPm && (Smax && Mmax && Hmax) || (Timeset && Hmax && Hrsadv) || (Timeset && Hmax && Mmax && Minadv));
+  ct_mod_N #(.N(7)) Dct(                          
+         .clk(Pulse), .rst(Reset), .en(Dayen), .ct_out(TDay), .z(Dmax)
+   );
+
+//  always_comb case(TDay) 
+//    3'b000 : DayLED = 7'b0111111;
+//    3'b001 : DayLED = 7'b1011111;
+//    3'b010 : DayLED = 7'b1101111;
+//    3'b011 : DayLED = 7'b1110111;
+//    3'b100 : DayLED = 7'b1111011;
+//    3'b101 : DayLED = 7'b1111101;
+//    3'b110 : DayLED = 7'b1111110;
+//    default : DayLED = 7'h00;
+//  endcase
+
+	assign DayLED = TDay == 0 ? 7'b1000000 : TDay == 1 ? 7'b0100000 : TDay == 2 ? 7'b0010000 : TDay == 3 ? 7'b0001000 :
+						      TDay == 4 ? 7'b0000100 : TDay == 5 ? 7'b0000010 : TDay == 6 ? 7'b0000001 : 7'h00;
 endmodule
